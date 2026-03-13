3db8ef69f9b4accd2e6d19a9565bb46ea7a5b671
Smarty Indonesia <smartyindo@gmail.com>
2026-03-13 16:33:28 +0700

    update

---
+++ api/include/rolling-banner.php
@@ -0,0 +1,48 @@
+    <div id="container">
+      <div class="scroll" id="scroll-one">
+        <div class="item">
+          <p> DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+        <div class="item">
+          <p> $DURIAN IS STINKY FRUIT</p>
+        </div>
+        <div class="item"><p><img src="/assets/image/rolling-banner/durian.png" width="25px"></p></div>
+      </div>
+      <div class="fade"></div>
+
+
+    </div>
+
