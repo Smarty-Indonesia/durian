66b36bcd4a97492308babea524ac48315e2443bf
Smarty Indonesia <smartyindo@gmail.com>
2026-03-13 12:21:09 +0700

    initial commit

---
+++ include/foot.php
@@ -0,0 +1,12 @@
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+
+    <script>
+      $(window).on("load", function () {
+        $("#consentModal").modal("show");
+      });
+    </script>
---
+++ include/footer.php
@@ -0,0 +1,12 @@
+      <div class="container">
+        <p class="float-end mb-1">
+          <a href="#">Back to top</a>
+        </p>
+        <p class="mb-1">
+          Durian Token Meme is &copy; ISPYN, to buy and to know more visit our homepage!
+        </p>
+        <p class="mb-0">
+          New to Crypto? <a href="/">Visit the homepage</a> or read our
+          <a href="/">getting started guide</a>.
+        </p>
+      </div>
